ced19a4040c314cbf15270a0680dbf513fc53aac
Angel Salazar <aangelsalazarr@gmail.com>
2022-11-14 22:50:49 -0500

    first graph

---
+++ data_processor.py
@@ -0,0 +1,69 @@
+import requests
+import json
+import os
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+# want to load into our environment our api key
+simbolico = os.environ.get('inegiKey')
+
+# dictionary storing possible request types
+indicators = {'510108':'producto interno bruto (total nacional)',
+              '497676':'actividad turistica, base 2013, indices de vol fisico',
+              '1002000001':'poblacion total'}
+pop_migration = {
+    '6200205255': 'Immigration Population, 5 yrs and over',
+    '6200205252': '% international migran pop. headed to other country',
+    '6200205262': 'International migration net balance of 5 yrs and over',
+    '6200205264': '% international migrant with no destination',
+    '6200240349': '% international migration to USA',
+    '6200240327': '% of international migration to rest of world',
+    '6200240404': '% international migrant pop. due to public '
+                  'insecurity/violence',
+    '6200240415': '% of international migrant pop. to unspecified countries',
+    '6207129645': '% migrant pop. of 5 yrs and over according to: education',
+    '6207129646': '% migrant pop. of 5 yrs or over according to: criminal '
+                  'insecurity or violence'
+}
+
+
+geos = {'0700':'nacional',
+        '07000001': 'not sure what this is'}
+
+bridges = {'BIE':'Bank for Economic Information',
+           'BISE':'Bank of Indicators'}
+
+
+def grab_inegi_data(indicator, geo, bridge):
+    # calling api
+    base = 'https://en.www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/'
+    ind = indicator + '/'  # id indicator, list(mining.keys())[6]
+    lang = 'en' + '/'  # or 'es' for spanish language
+    geo_id = geo + '/'  # geographic area, list(geos.keys())[1]
+    recents = 'false/'  # or 'false/' for if you want historical data?
+    data_bridge = bridge + '/'  # list(bridges.keys())[1]
+    v = '2.0/'  # not sure what else you could put here for version lol
+    token = simbolico + '?type=json'  # api key
+    # url segments concatenated
+    url = base + ind + lang + geo_id + recents + data_bridge + v + token
+
+    # response var
+    response = requests.get(url=url)
+    data = response.json()
+    entries = data['Series'][0]['OBSERVATIONS']
+    df = pd.DataFrame(data=entries)
+    df['TIME_PERIOD'] = pd.to_datetime(df['TIME_PERIOD'])
+    df['OBS_VALUE'] = df['OBS_VALUE'].astype(float)
+
+    return df
+
+
+def save_multi_image(filename):
+    pp = PdfPages(filename)
+    fig_nums = plt.get_fignums()
+    figs = [plt.figure(n) for n in fig_nums]
+    for fig in figs:
+        fig.savefig(pp, format='pdf')
+    pp.close()
---
+++ inegi_mining.py
@@ -0,0 +1,90 @@
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+from data_processor import grab_inegi_data, save_multi_image
+import pandas as pd
+from datetime import date
+
+# want to load into our environment our api key
+simbolico = os.environ.get('inegiKey')
+# purpose is to create current date and current year function
+today = date.today()
+currentDate = today.strftime('%m_%d_%y')
+# setting up params for our data_visuals
+sns.set(font_scale=0.5)
+sns.set_style('dark')
+
+# geo identifier
+geos = {'0700':'nacional',
+        '07000001': 'not sure what this is'}
+
+# data bridges
+bridges = {'BIE':'Bank for Economic Information',
+           'BISE':'Bank of Indicators'}
+
+# mining ids
+mining = {
+    '5300000005': 'Economic units, sector 21, mining',
+    '5300000025': 'Total renumeration, sector 21, mining',
+    '5300000035': 'Total gross production, sector 21, mining',
+    '5300000045': 'Total stock of fixed assets, sector 21, mining',
+    '5300000095': 'Recenue from provision of goods and services, sector 21, '
+                  'mining',
+    '5300000105': 'Total expenditures for consumption of goods and services, '
+                  'sector 21, mining',
+    '6207046219': 'Volume of mining production, Gold, by state',
+    '6207046220': 'Volume of mining production, Zinc, by state',
+    '6207046221': 'Volume of mining production, Copper, by state',
+    #'6207046222': 'Volume of mining production, Coke, by state',
+    '6207046224': 'Volume of mining production, Silver by state',
+    '6207046223': 'Volume of mining production, Lead, by state',
+    #'6207046225': 'Volume of mining production, Sulfur, by state',
+    #'6207046226': 'Volume of mining production, Baryte, by state',
+    #'6207046227': 'Volume of mining production, Fluorite, by state',
+    #'6207046230': 'Volume of mining production, Iron Pellets, by state',
+    #'6207046231': 'Volume of mining production, Iron Extraction by state',
+}
+
+# creating a list of keys
+min_keys = list(mining.keys())
+geo_keys = list(geos.keys())
+bridge_keys = list(bridges.keys())
+
+# turning this into a function moving forward
+df = pd.DataFrame(columns=['seriesId'])
+
+# now let's iterate through each key:value pair in dict and create a df
+for key in min_keys:
+
+    # grab data
+    a = grab_inegi_data(indicator=key, geo=geo_keys[1], bridge=bridge_keys[1])
+    df = pd.concat([df, pd.DataFrame(a)])
+
+    # add series id
+    if df['seriesId'].isnull:
+        df['seriesId'].fillna(key, inplace=True)
+
+
+# let's drop cols = OBS_EXCEPTION, OBS_SOURCE, & OBS_NOTE
+df.drop(['OBS_EXCEPTION', 'OBS_STATUS', 'OBS_SOURCE', 'OBS_NOTE'],
+        inplace=True, axis=1)
+
+# lets output our df as a csv file
+df.to_csv('./data_files/mx_mining_visuals.csv', index=False)
+
+# now let's iterate visualizing
+for key, value in mining.items():
+    fig = plt.subplot()
+    sns.lineplot(data=df[df['seriesId'] == key],
+                 x='TIME_PERIOD',
+                 y='OBS_VALUE').set(title=value)
+
+
+
+
+# now we will convert our figures into a pdf file
+filename = './data_visuals/mexico_mining_data_visuals'
+save_multi_image(filename + currentDate + '.pdf')
+
+
+
