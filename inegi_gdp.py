6c4b51f320ef1113363586d1a34d9a3aac0a6e35
Angel Salazar <aangelsalazarr@gmail.com>
2022-11-27 10:21:39 -0500

    inegi gdp code reformatted

--- inegi_gdp.py
+++ inegi_gdp.py
@@ -2,7 +2,7 @@ import os
 import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib import rc
-from data_processor import grab_inegi_data, save_multi_image
+from data_processor import *
 import pandas as pd
 from datetime import date
 
@@ -21,15 +21,36 @@ sns.set(font_scale=0.5)
 sns.set_style('dark')
 
 # geo identifier
-geos = {'0700':'nacional',
+geos = {'0700': 'nacional',
         '07000001': 'not sure what this is'}
 
 # data bridges
-bridges = {'BIE':'Bank for Economic Information',
-           'BISE':'Bank of Indicators'}
+bridges = {'BIE': 'Bank for Economic Information',
+           'BISE': 'Bank of Indicators'}
 
 # 0-3 are BISE, 4-6 are BIE, both geos =0700
-gdps_1 = {
+gdps1 = {
+    '6207067864': 'Timely estimate of the quarterly GDP. Annex 1. '
+                  'Gross Domestic Product. Originals. Annual Percentage '
+                  'Variation',
+    '6207067865': 'Timely estimate of the quarterly GDP. Annex 1. '
+                  'Gross Domestic Product. Primary Activities. Originals. '
+                  'Annual Percentage Variation',
+    '6207067866': 'Timely estimate of the quarterly GDP. Annex 1. '
+                  'Gross Domestic Product. Tertiary Activities. Originals. '
+                  'Annual percentage variation',
+    '6207067867': 'Timely estimate of the quarterly GDP. Annex 1. '
+                  'Gross Domestic Product. Secondary Activities. '
+                  'Annual percentage variation '
+}
+
+gdps2 = {
+    '494269': 'Producto Interno Bruto, a precios de mercado',
+    '494270': 'Impuestos a los productos, netos',
+    '494271': 'Valor agregado bruto a precios basicos',
+}
+
+gdps = {
     '6207067864': 'Timely estimate of the quarterly GDP. Annex 1. '
                   'Gross Domestic Product. Originals. Annual Percentage '
                   'Variation',
@@ -42,61 +63,47 @@ gdps_1 = {
     '6207067867': 'Timely estimate of the quarterly GDP. Annex 1. '
                   'Gross Domestic Product. Secondary Activities. '
                   'Annual percentage variation ',
-}
-
-gdps_2 = {
     '494269': 'Producto Interno Bruto, a precios de mercado',
     '494270': 'Impuestos a los productos, netos',
-    '494271': 'Valor agregado bruto a precios basicos',
+    '494271': 'Valor agregado bruto a precios basicos'
 }
 
 # creating a list of keys
-gdp_keys1 = list(gdps_1.keys())
-gdp_keys2 = list(gdps_2.keys())
+gdp1_keys = list(gdps1.keys())
+gdp2_keys = list(gdps2.keys())
 geo_keys = list(geos.keys())
 bridge_keys = list(bridges.keys())
 
-# turning this into a function moving forward
-df = pd.DataFrame(columns=['seriesId'])
+# creating first df for first dict
+df1 = process_data_by_series(keys=gdp1_keys, geo=geo_keys[0],
+                             bridge=bridge_keys[1])
 
-# now let's iterate through each key:value pair in dict and create a df
-for key in gdp_keys2:
+# creating second df for second dict
+df2 = process_data_by_series(keys=gdp2_keys, geo=geo_keys[0],
+                             bridge=bridge_keys[0])
 
-    # grab data
-    a = grab_inegi_data(indicator=key, geo=geo_keys[0], bridge=bridge_keys[0])
-    df = pd.concat([df, pd.DataFrame(a)])
+#
+dfs = [df1, df2]
 
-    # add series id
-    if df['seriesId'].isnull:
-        df['seriesId'].fillna(key, inplace=True)
+# combing our dfs
+df_main = combine_dfs(dfs=dfs)
 
+# cleaning our data
+df_main = clean_data(df=df_main)
 
-# let's drop cols = OBS_EXCEPTION, OBS_SOURCE, & OBS_NOTE
-df.drop(['OBS_EXCEPTION', 'OBS_STATUS', 'OBS_SOURCE', 'OBS_NOTE', 'COBER_GEO'],
-        inplace=True, axis=1)
-
+'''
 # TIME_PERIOD -> (1) Year & (2) Quarter
 df['year'] = df['TIME_PERIOD'].str[:4]
 df['quarter'] = df['TIME_PERIOD'].str[-1:]
 df['period'] = df['year'] + '-Q' + df['quarter']
 df['period'] = pd.to_datetime(df['period']).dt.strftime('%Y-%m')
-
-
-# converting OBS_VALUE to float
-df['OBS_VALUE'] = df['OBS_VALUE'].astype(float)
+'''
 
 # lets output our df as a csv file
-df.to_csv('./data_files/mx_gdp_data.csv', index=False)
+df_main.to_csv('./data_files/mx_gdp_data.csv', index=False)
 
-# now let's iterate visualizing
-for key, value in gdps_2.items():
-    plt.figure()
-    x = sns.barplot(data=df[(df['seriesId'] == key) & (df['period'] >=
-                                                       '2015-01-01')],
-                    x='period',
-                    y='OBS_VALUE').set(title=value)
-    # set the ticks first
-    plt.xticks(rotation=90)  # Rotates X-Axis Ticks by 45-degrees
+# plotting our data
+vis_data_barplot(dict=gdps, df=df_main, start='2015-01')
 
 # now we will convert our figures into a pdf file
 filename = './data_visuals/mexico_gdp_data_visuals_'
